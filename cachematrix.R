529ed3f2b8a4d1f219a3669b945734fb6f00a2a8
mlrn <mlrn@users.noreply.github.com>
2016-08-14 23:09:27 +0200

    Last commit

--- cachematrix.R
+++ cachematrix.R
@@ -33,7 +33,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## via the setinverse function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x' 
         m <- x$getinverse()
         if (!is.null(m)){
                 message("getting cached matrix")
